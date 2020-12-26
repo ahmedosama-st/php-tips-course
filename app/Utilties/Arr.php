860c3877cbdfd8d834b870e9b41bccdcc675aa43
ahmedosama-st <marvelphp5@gmail.com>
2020-12-26 13:47:29 +0200

    Initializing project

---
+++ app/Utilties/Arr.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Utilties;
+
+class Arr
+{
+    public static function flatten($array, $depth = INF)
+    {
+        $result = [];
+
+        foreach ($array as $item) {
+            $item = $item instanceof Collection ? $item->all() : $item;
+
+            if (!is_array($item)) {
+                $result[] = $item;
+            } else {
+                $values = $depth === 1 ? array_values($item) : static::flatten($item, $depth - 1);
+
+                foreach ($values as $value) {
+                    $result[] = $value;
+                }
+            }
+        }
+
+        return $result;
+    }
+}
--- src/utilities/Zipper.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Utilities;
-
-class Zipper
-{
-}
